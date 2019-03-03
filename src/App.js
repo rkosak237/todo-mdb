2a54603203686929ad2ec32fec96f24f96f05437
Robert <robert.kosak237@gmail.com>
2019-03-03 15:41:49 +0100

    fixed index.html. It was creating problem after deployment - 404 on gh - pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: [{id: 1, text:'Barnabus Stinson!'}, {id: 2, text: 'Buy travel & tour guide book'}, {id: 3, text: 'Be awesome'}, {id: 4, text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}, { id: 5, text:'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes,'}],
+      tasks: [],
     }
   }
 
