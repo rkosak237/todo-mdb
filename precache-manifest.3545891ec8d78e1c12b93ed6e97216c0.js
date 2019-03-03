1a06a36f0cb377f1a0961d8bc41889e81e34a8ee
Robert <robert.kosak237@gmail.com>
2019-03-03 15:23:40 +0100

    Updates

---
+++ precache-manifest.3545891ec8d78e1c12b93ed6e97216c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7204ca927a43be6d58a3",
+    "url": "/todo-mdb/static/js/runtime~main.7204ca92.js"
+  },
+  {
+    "revision": "f0e9f13f0906aaaa5bc0",
+    "url": "/todo-mdb/static/js/main.f0e9f13f.chunk.js"
+  },
+  {
+    "revision": "93fb9c3d85cd4e8e7cd4",
+    "url": "/todo-mdb/static/js/2.93fb9c3d.chunk.js"
+  },
+  {
+    "revision": "f0e9f13f0906aaaa5bc0",
+    "url": "/todo-mdb/static/css/main.8c367191.chunk.css"
+  },
+  {
+    "revision": "777fcec241bebb16ce03113fcef38eee",
+    "url": "/todo-mdb/index.html"
+  }
+];
